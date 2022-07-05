be958d34b32741f4dfb5954155f387863e06b6a4
fieldOfView <aldo@fieldofview.com>
2022-07-05 08:47:59 +0200

    Simplify SensorManager class

--- SensorManager.py
+++ SensorManager.py
@@ -11,73 +11,45 @@ from PySide2.QtCore import QObject, Property
 class SensorManager(QObject):
     def __init__(self):
         QObject.__init__(self)
-        self._spaceStateSensor = None
-        self._weatherSensor = None
-        self._ikeaSensor = None
-        self._jimSensor = None
-        self._pimSensor = None
-        self._rpiSensor = None
+        self._sensors = {}
 
     def stop(self):
-        if self._spaceStateSensor:
-            self._spaceStateSensor.stop()
-        if self._weatherSensor:
-            self._weatherSensor.stop()
-        if self._ikeaSensor:
-            self._ikeaSensor.stop()
+        for className in self._sensors:
+            self._sensors[className].stop()
 
-        if self._jimSensor:
-            self._jimSensor.stop()
-        if self._pimSensor:
-            self._pimSensor.stop()
-        if self._rpiSensor:
-            self._rpiSensor.stop()
-
+    def getSensor(self, cls):
+        className = cls.__name__
+        if className not in self._sensors:
+            self._sensors[className] = cls()
+            self._sensors[className].start()
+        return self._sensors[className]
 
     def SpaceState(self) -> SpaceStateSensor:
-        if not self._spaceStateSensor:
-            self._spaceStateSensor = SpaceStateSensor()
-            self._spaceStateSensor.start()
-        return self._spaceStateSensor
+        return self.getSensor(SpaceStateSensor)
 
     SpaceState = Property(QObject, fget=SpaceState, constant=True)
 
     def Weather(self) -> WeatherSensor:
-        if not self._weatherSensor:
-            self._weatherSensor = WeatherSensor()
-            self._weatherSensor.start()
-        return self._weatherSensor
+        return self.getSensor(WeatherSensor)
 
     Weather = Property(QObject, fget=Weather, constant=True)
 
     def IKEA(self) -> IKEASensor:
-        if not self._ikeaSensor:
-            self._ikeaSensor = IKEASensor()
-            self._ikeaSensor.start()
-        return self._ikeaSensor
+        return self.getSensor(IKEASensor)
 
     IKEA = Property(QObject, fget=IKEA, constant=True)
 
     def Jim(self) -> JimSensor:
-        if not self._jimSensor:
-            self._jimSensor = JimSensor()
-            self._jimSensor.start()
-        return self._jimSensor
+        return self.getSensor(JimSensor)
 
     Jim = Property(QObject, fget=Jim, constant=True)
 
     def Pim(self) -> PimSensor:
-        if not self._pimSensor:
-            self._pimSensor = PimSensor()
-            self._pimSensor.start()
-        return self._pimSensor
+        return self.getSensor(PimSensor)
 
     Pim = Property(QObject, fget=Pim, constant=True)
 
     def RPI(self) -> RPITelemetrySensor:
-        if not self._rpiSensor:
-            self._rpiSensor = RPITelemetrySensor()
-            self._rpiSensor.start()
-        return self._rpiSensor
+        return self.getSensor(RPITelemetrySensor)
 
     RPI = Property(QObject, fget=RPI, constant=True)
